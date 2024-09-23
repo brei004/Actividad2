be57bd982a53a9a2ebf6ead9cb3321613662243f
brei004 <altherego04@gmail.com>
2024-09-23 09:34:11 -0500

    Cuatro

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,11 @@
 const request = require('supertest');
 const app = require('../src/app');
-let server;
-let address;
-
-beforeAll((done) => {
-  server = app.listen(0, () => {
-    address = server.address();
-    done();
-  });
-});
-
-afterAll((done) => {
-  server.close(done);  // Cierra el servidor al terminar las pruebas
-});
 
 describe('GET /', () => {
-  it('should return Hello, World!', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello, World!');
+    it('should return Hello, World!', async () => {
+      const res = await request(app).get('/');
+      expect(res.statusCode).toEqual(200);
+      expect(res.text).toBe('Hello, World!');
+    }, 10000);  // 10 segundos de timeout
   });
-});
+  
