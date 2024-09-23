db5ae5c3d0b7685f0a4017a8d716ae9b70180a91
brei004 <altherego04@gmail.com>
2024-09-23 09:42:44 -0500

    Cinco

--- src/app.js
+++ src/app.js
@@ -5,9 +5,14 @@ app.get('/', (req, res) => {  // Ruta GET
     res.send('Hello, World!'); // Respuesta GET
 });
 
-const port = process.env.PORT || 3000; // Puerto
+const port = process.env.PORT || 3003; // Puerto
 app.listen(port, () => {    // Inicia servidor
     console.log(`Server running on port ${port}`);
 });
-
+const timeoutDuration = 5000; // Duración en milisegundos (5 segundos)
+setTimeout(() => {
+    server.close(() => {
+        console.log('Server closed after timeout');
+    });
+}, timeoutDuration);
 module.exports = app;   // Exporta app
--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,21 @@
 const request = require('supertest');
 const app = require('../src/app');
+let server;
+
+beforeAll((done) => {
+  server = app.listen(3000, () => {  // Inicia el servidor en el puerto 3000
+    done();
+  });
+});
+
+afterAll((done) => {
+  server.close(done);  // Cierra el servidor al terminar las pruebas
+});
 
 describe('GET /', () => {
-    it('should return Hello, World!', async () => {
-      const res = await request(app).get('/');
-      expect(res.statusCode).toEqual(200);
-      expect(res.text).toBe('Hello, World!');
-    }, 10000);  // 10 segundos de timeout
+  it('should return Hello, World!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello, World!');
   });
-  
+});
