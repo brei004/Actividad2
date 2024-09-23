7f2f8d3af7584116643929721c545628c085a585
brei004 <altherego04@gmail.com>
2024-09-23 09:32:29 -0500

    Tres

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,23 @@
 const request = require('supertest');
 const app = require('../src/app');
+let server;
+let address;
 
 beforeAll((done) => {
-    server = app.listen(3000, done);  // Inicia el servidor para las pruebas
+  server = app.listen(0, () => {
+    address = server.address();
+    done();
   });
-  
-  afterAll((done) => {
-    server.close(done);  // Cierra el servidor al terminar las pruebas
-  });
-  
+});
 
-// prueba para la ruta GET '/'
+afterAll((done) => {
+  server.close(done);  // Cierra el servidor al terminar las pruebas
+});
+
 describe('GET /', () => {
-    it('should return Hello, World!', async () => {
-        // solicitud GET a '/'
-        const res = await request(app).get('/');
-        // verificación de respuesta código 200
-        expect(res.statusCode).toEqual(200);
-        // verificación del cuerpo de la puesta
-        expect(res.text).toBe('Hello, World!');
-    });
+  it('should return Hello, World!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello, World!');
+  });
 });
