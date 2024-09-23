2802d9692defb12e2e17718c1b799353af3c5a2a
brei004 <altherego04@gmail.com>
2024-09-23 09:30:28 -0500

    Dos

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,15 @@
 const request = require('supertest');
 const app = require('../src/app');
 
+beforeAll((done) => {
+    server = app.listen(3000, done);  // Inicia el servidor para las pruebas
+  });
+  
+  afterAll((done) => {
+    server.close(done);  // Cierra el servidor al terminar las pruebas
+  });
+  
+
 // prueba para la ruta GET '/'
 describe('GET /', () => {
     it('should return Hello, World!', async () => {
